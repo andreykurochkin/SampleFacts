23bdb6be58fb9384ffe1db9ceca1ee5bff3d59cd
andreykurochkin <andrey.kurochkin@hotmail.com>
2021-11-10 18:38:49 +0300

    project up and ready for entities

---
+++ src/Kurochkin.SampleFacts/Kurochkin.SampleFacts.Web/Data/DataInitializer.cs
@@ -0,0 +1,85 @@
+﻿using Kurochkin.SampleFacts.Web.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Calabonga.Microservices.Core.Exceptions;
+namespace Kurochkin.SampleFacts.Web.Data
+{
+    public static class DataInitializer
+    {
+        internal static async Task InitializeAsync(IServiceProvider serviceProvider)
+        {
+            var scope = serviceProvider.CreateScope();
+            await using var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
+            var exists = context!.GetService<IDatabaseCreator>() is RelationalDatabaseCreator databaseCreator 
+                &&  await databaseCreator.ExistsAsync();
+            if (exists)
+            {
+                return;
+            }
+
+            await context.Database.MigrateAsync();
+
+            var roles = AppData.Roles.ToArray();
+            var roleStore = new RoleStore<IdentityRole>(context);
+            foreach(var role in roles)
+            {
+                if (!context.Roles.Any(x => x.Name == role))
+                {
+                    await roleStore.CreateAsync(new IdentityRole(role) 
+                    { 
+                        NormalizedName = role.ToUpper() 
+                    });
+                }
+            }
+
+            const string userName = "dev@gmail.com";
+            if (context.Users.Any(x => x.Email == userName))
+            {
+                return;
+            }
+
+            var user = new IdentityUser
+            {
+                Email = userName,
+                EmailConfirmed = true,
+                NormalizedEmail = userName.ToUpper(),
+                PhoneNumber = "+7900000000",
+                UserName = userName,
+                PhoneNumberConfirmed = true,
+                NormalizedUserName = userName.ToUpper(),
+                SecurityStamp = Guid.NewGuid().ToString("D")
+            };
+
+            var passwordHasher = new PasswordHasher<IdentityUser>();
+            var passwordHash = passwordHasher.HashPassword(user, "123qwe!@#");
+
+            var userStore = new UserStore<IdentityUser>(context);
+            var identityResult = await userStore.CreateAsync(user);
+            if (!identityResult.Succeeded)
+            {
+                var message = string.Join(", ", identityResult.Errors.Select(x=>$"{x.Code}: {x.Description})"));
+                throw new MicroserviceDatabaseException(message);
+            }
+
+            var userManager = scope.ServiceProvider.GetService<UserManager<IdentityUser>>();
+            foreach (var role in roles)
+            {
+                var identityResultRole = await userManager!.AddToRoleAsync(user, role);
+                if (!identityResultRole.Succeeded)
+                {
+                    var message = string.Join(", ", identityResultRole.Errors.Select(x => $"{x.Code}: {x.Description})"));
+                    throw new MicroserviceDatabaseException(message);
+                }
+            }
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
---
+++ src/Kurochkin.SampleFacts/Kurochkin.SampleFacts.Web/Infrastructure/AppData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kurochkin.SampleFacts.Web.Infrastructure
+{
+    public static class AppData
+    {
+        public const string AdministratorRoleName = "Administrator";
+        public const string UserRoleName = "User";
+
+        public static IEnumerable<string> Roles
+        {
+            get
+            {
+                yield return AdministratorRoleName;
+                yield return UserRoleName;
+            }
+        }
+    }
+}
