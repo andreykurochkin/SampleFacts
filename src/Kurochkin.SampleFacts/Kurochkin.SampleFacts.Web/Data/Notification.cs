8a847e9ca6dd08a08659f70411b8830157ae4cdc
andreykurochkin <andrey.kurochkin@hotmail.com>
2021-11-10 23:32:23 +0300

    added entity Notification

---
+++ src/Kurochkin.SampleFacts/Kurochkin.SampleFacts.Web/Data/Notification.cs
@@ -0,0 +1,20 @@
+﻿using Calabonga.EntityFrameworkCore.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kurochkin.SampleFacts.Web.Data
+{
+    /// <summary>
+    /// Notification entity
+    /// </summary>
+    public class Notification : Auditable
+    {
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public bool IsCompleted { get; set; }
+        public string AddressFrom { get; set; }
+        public string AddressTo { get; set; }
+    }
+}
